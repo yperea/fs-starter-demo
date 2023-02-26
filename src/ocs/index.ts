a16183964811acba95c1ee48986282b6ec19ecb4
Yesid <yperea@icore.co>
2023-02-26 10:50:00 -0600

    Moving console log for scrolling from lenis.ts to index.ts.

--- src/ocs/index.ts
+++ src/ocs/index.ts
@@ -1,11 +1,16 @@
 import { greetUser } from '$utils/greet';
-import { raf } from '$utils/lenis';
+import { lenis, raf } from '$utils/lenis';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'One City Schools';
 
   greetUser(name);
+
+  //get scroll value
+  lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+    console.log({ scroll, limit, velocity, direction, progress });
+  });
 
   requestAnimationFrame(raf);
 });
--- src/utils/lenis.ts
+++ src/utils/lenis.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import Lenis from '@studio-freight/lenis';
 
-const lenis = new Lenis({
+export const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
   direction: 'vertical', // vertical, horizontal
@@ -13,12 +13,7 @@ const lenis = new Lenis({
   infinite: false,
 });
 
-//get scroll value
-lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
-  console.log({ scroll, limit, velocity, direction, progress });
-});
-
-export function raf(time) {
+export function raf(time: number) {
   lenis.raf(time);
   requestAnimationFrame(raf);
 }
