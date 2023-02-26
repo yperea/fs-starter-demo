6e53ff8b92275c043648ce03306ac2ec752ebce0
Yesid <yperea@icore.co>
2023-02-26 10:42:23 -0600

    Adding Lenis library for smooth scrolling.

--- src/ocs/index.ts
+++ src/ocs/index.ts
@@ -1,7 +1,11 @@
 import { greetUser } from '$utils/greet';
+import { raf } from '$utils/lenis';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'One City Schools';
+
   greetUser(name);
+
+  requestAnimationFrame(raf);
 });
